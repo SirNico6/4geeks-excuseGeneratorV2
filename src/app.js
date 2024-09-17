b5f68acf9bcd4641555b33dd65d8b72069d0b3b2
Nicolas Arevalo <gabrielnicolas512@outlook.com>
2024-09-16 22:43:06 -0300

    first commite

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car", "my bed"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch"
+  ];
+
+  const getRandomNum = () => {
+    return Math.floor(Math.random() * 4);
+  };
+
+  let excuse = `${who[getRandomNum()]} ${action[getRandomNum()]} ${
+    what[getRandomNum()]
+  } ${when[getRandomNum()]}`;
+
+  document.getElementById("excuse").innerText = excuse;
 };
